2c6963b0605750f3183b134ebb84afcc2e951b5d
Ben Ricker <BGRicker@gmail.com>
2017-02-19 21:40:00 -0500

    lodash/package.json

--- notes/app.js
+++ notes/app.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+console.log(_.isString(true));
+console.log(_.isString('true'));
+console.log(_.uniq([2, 1, 2]));
 
 console.log('result: ', notes.add(9, -2));
 
