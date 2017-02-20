8c79ff3abbec7f5d4993db34bcd82d96fba0adc7
Ben Ricker <BGRicker@gmail.com>
2017-02-20 16:15:37 -0500

    real deal functions

--- notes/notes.js
+++ notes/notes.js
@@ -2,7 +2,25 @@ console.log('starting notes.js');
 
 // module.exports.age = 30;
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note: ';
-}
+var addNote = (title, body) => {
+  console.log('Adding note', title," - ", body); 
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log(`Getting note: ${title}`);
+};
+
+var removeNote = (title) => {
+  console.log(`Removing note: ${title}`);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
