0f30cf7b586459189073ec055b0b33283c5574d3
Ben Ricker <BGRicker@gmail.com>
2017-02-19 22:43:50 -0500

    user args, they stink right now

--- notes/app.js
+++ notes/app.js
@@ -1,20 +1,22 @@
+console.log('Starting app.js');
+
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('true'));
-console.log(_.uniq([2, 1, 2]));
+var command = _.lowerCase(process.argv[2])
+console.log('Command: ', command);
+console.log(process.argv)
 
-console.log('result: ', notes.add(9, -2));
-
-// var user = os.userInfo();
-//
-// fs.appendFile('hi.md', `Hi there ${user.username}, you are ${notes.age}\n`, function (err) {
-//   if (err) {
-//     console.log("couldn't write the file");
-//   }
-// });
-
-
+if (command === 'add') {
+  console.log("Adding note");
+} else if (command === 'list') {
+  console.log("Listing all notes");
+} else if (command === 'read') {
+  console.log("Reading note");
+} else if (command === 'remove') {
+  console.log("Removing note");
+} else {
+  console.log('Command not recognized');
+}
--- notes/hi.md
+++
@@ -1,6 +0,0 @@
-Hi there
-Hi there bricker
-Hi there bricker, you are 30
-Result: 9 - 12
-
-21
